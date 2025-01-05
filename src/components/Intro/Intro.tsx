4f56d57fa394f5daedfb2b1c10d105dab605e354
Pedro Delgadillo <pedelgadillo@gmail.com>
2025-01-05 00:12:17 -0600

    create intro section tsx and scss

---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,48 @@
+import "./intro.scss";
+import { ReactTyped } from "react-typed";
+
+interface IntroProps {
+  menuOpen: boolean;
+}
+
+const Intro: React.FC<IntroProps> = ({ menuOpen }) => {
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className={"imgContainer " + (menuOpen && "active")}>
+          <img
+            src="assets/pedro.png"
+            alt="Pedro smiling"
+          />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello! I'm</h2>
+          <h1>Pedro Delgadillo</h1>
+          <h3>
+            Looking to hire a{" "}
+            <ReactTyped
+              strings={[
+                "Software Engineer",
+                "Full-Stack Developer",
+                "Java Developer",
+                "DevOps Engineer",
+              ]}
+              typeSpeed={40}
+              backSpeed={50}
+              backDelay={1000}
+              startDelay={500} 
+              showCursor={false} 
+            />
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down arrow" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
