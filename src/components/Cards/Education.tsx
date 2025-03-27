09cdd4ef0be7cd61c2d110b7c6fbfb3dd4293a0b
Pete D <pedelgadillo@gmail.com>
2025-03-27 13:21:08 -0500

    feat: Add Windows 2000-style terminal component with neon theme

--- src/components/Cards/Education.tsx
+++ src/components/Cards/Education.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
 import TerminalCard from './TerminalCard';
-import { resumeData } from '../../constants/resume/resumeData';
-import { Education } from '../../constants/resume/resumeDataTypes';
 
-const EducationCard: React.FC = () => {
-    const formattedEducation = resumeData.education.map((edu: Education) => ({
-        title: edu.institution,
-        subtitle: edu.degree,
-        duration: edu.duration,
-        items: edu.details,
-        honors: edu.honors ? {
-            label: "Honors",
-            value: edu.honors
-        } : undefined
-    }));
-
-    return <TerminalCard items={formattedEducation} type="education" />;
+const Education: React.FC = () => {
+    return <TerminalCard type="education" title="Education" />;
 };
 
-export default EducationCard; 
+export default Education; 
